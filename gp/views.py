0568a0b80214da6eeb37f1f084ec465dd5ffd965
David Herron <herrondavid1@gmail.com>
2016-08-03 12:07:26 -0500

    Modified templates

--- gp/views.py
+++ gp/views.py
@@ -6,16 +6,16 @@ from .models import Recipe, Feature
 # Create your views here.
 
 def features(request):
-    # latest_feature = Feature.objects.order_by('-id')[0]
-    # second_feature = Feature.objects.order_by('-id')[1]
-    # third_feature = Feature.objects.order_by('-id')[2]
-    # fourth_feature = Feature.objects.order_by('-id')[3]
-    # fifth_feature = Feature.objects.order_by('-id')[4]
-    # past_features = Feature.objects.all().order_by('-pub_date')
-    # context = {'latest_feature': latest_feature,
-    #            'past_features': past_features,
-    #            'second_feature': second_feature,
-    #            'third_feature': third_feature,
-    #            'fourth_feature': fourth_feature,
-    #            'fifth_feature': fifth_feature}
-    return render(request, 'gp/features.html')
+    latest_feature = Feature.objects.order_by('-id')[0]
+    second_feature = Feature.objects.order_by('-id')[1]
+    third_feature = Feature.objects.order_by('-id')[2]
+    fourth_feature = Feature.objects.order_by('-id')[3]
+    fifth_feature = Feature.objects.order_by('-id')[4]
+    past_features = Feature.objects.all().order_by('-pub_date')
+    context = {'latest_feature': latest_feature,
+               'past_features': past_features,
+               'second_feature': second_feature,
+               'third_feature': third_feature,
+               'fourth_feature': fourth_feature,
+               'fifth_feature': fifth_feature}
+    return render(request, 'gp/features.html', context)
